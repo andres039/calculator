839b19e27a09bae08433e1b6d74283e991abdff9
Andres <andres039039@hotmail.com>
2021-08-09 07:50:26 -0400

    Added regular functionality to the calculator

--- script.js
+++ script.js
@@ -2,11 +2,52 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
-const operate = (operator, x, y) =>
-  operator === "+"
-    ? add(x, y)
-    : operator === "-"
-    ? subtract(x, y)
-    : operator === "*"
-    ? multiply(x, y)
-    : divide(x, y);
+//use the 'display value' as the values for the 'operate' function 
+const operate = (x, z, y) =>
+{   
+  if (z === '+') {
+    return add(x, y)
+}  else if (z === "-") {
+    return subtract(x, y)
+  } else if (z=== "*") {
+    return multiply(x, y)
+  }
+  else {
+    return divide(x, y)
+  }
+}
+const keys = [...document.querySelectorAll(".keys")]
+const display = document.querySelector('#display')
+keys.forEach(key => {
+  if (key.innerText === '=') {} else {
+  key.addEventListener('click', updateDisplay)
+  }
+})
+function updateDisplay (event) {
+  if (display.innerText === '0') {
+  display.innerText = event.target.innerText
+  } else {
+    display.innerText += event.target.innerText
+  }
+ }
+
+function defineOperation () {
+  let stringToOperate = display.innerText
+  let arrayToWork = [...stringToOperate]
+  let sign = arrayToWork.filter(x => x === '+' || x === '-' || x === '*' ||x === '/')
+  let signIndex = arrayToWork.indexOf(sign[0])
+  let firstNumber = stringToOperate.slice(0, signIndex)
+  let secondNumber = stringToOperate.slice(signIndex + 1, stringToOperate.length)
+  display.innerText = operate(+firstNumber, sign[0], +secondNumber)
+  firstNumber = display.innerText
+}
+let equal = document.querySelector('#equal')
+equal.addEventListener('click', defineOperation)
+let operator = [...document.querySelectorAll('.operators')]
+
+
+
+
+
+
+
